361ef9d71f44b8a12160320c6d55f18d6da85593
nbrisola-website <142646566+nbrisola-website@users.noreply.github.com>
2023-08-19 07:23:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,59 @@
 const CLIENT_ID = 'YOUR_CLIENT_ID';
 const SCOPES = 'https://www.googleapis.com/auth/spreadsheets';
+const SPREADSHEET_ID = 'YOUR_SPREADSHEET_ID';
 
+const authButton = document.getElementById('authorizeButton');
 const expenseForm = document.getElementById('expenseForm');
 const expenseNameInput = document.getElementById('expenseName');
 const expenseAmountInput = document.getElementById('expenseAmount');
 const expenseList = document.getElementById('expenseList');
+let gapi;
 
-let sheetId = ''; // Your Google Sheet ID
-
-// Load the Google Sheets API
 function handleClientLoad() {
+  gapi = window.gapi;
   gapi.load('client:auth2', initClient);
 }
 
-// Initialize the Google Sheets API
 function initClient() {
   gapi.client.init({
     clientId: CLIENT_ID,
     scope: SCOPES,
     discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4']
   }).then(() => {
-    gapi.auth2.getAuthInstance().signIn().then(() => {
-      listExpenses();
-    });
+    if (!gapi.auth2.getAuthInstance().isSignedIn.get()) {
+      authButton.style.display = 'block';
+      authButton.onclick = handleAuthClick;
+    } else {
+      loadExpenses();
+      authButton.style.display = 'none';
+    }
   });
 }
 
-// Add an expense
+function handleAuthClick() {
+  gapi.auth2.getAuthInstance().signIn().then(() => {
+    authButton.style.display = 'none';
+    loadExpenses();
+  });
+}
+
 function addExpense(name, amount) {
   const values = [[name, amount]];
   gapi.client.sheets.spreadsheets.values.append({
-    spreadsheetId: sheetId,
+    spreadsheetId: SPREADSHEET_ID,
     range: 'Sheet1', // Update with your sheet name
     valueInputOption: 'USER_ENTERED',
     resource: {
       values: values
     }
   }).then(() => {
-    listExpenses();
+    loadExpenses();
   });
 }
 
-// List all expenses
-function listExpenses() {
+function loadExpenses() {
   gapi.client.sheets.spreadsheets.values.get({
-    spreadsheetId: sheetId,
+    spreadsheetId: SPREADSHEET_ID,
     range: 'Sheet1' // Update with your sheet name
   }).then(response => {
     const values = response.result.values;
@@ -61,7 +70,6 @@ function listExpenses() {
   });
 }
 
-// Submit form and add expense
 expenseForm.addEventListener('submit', e => {
   e.preventDefault();
   const name = expenseNameInput.value;
@@ -71,5 +79,4 @@ expenseForm.addEventListener('submit', e => {
   expenseAmountInput.value = '';
 });
 
-// Load Google API
 gapi.load('client:auth2', handleClientLoad);
